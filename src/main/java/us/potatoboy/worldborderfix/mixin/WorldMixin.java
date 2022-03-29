74c493284a4481a86f9260be11b1155e617a52f4
PotatoPresident <51728317+PotatoPresident@users.noreply.github.com>
2022-03-28 18:21:50 -0700

    Update to 1.18.2

--- src/main/java/us/potatoboy/worldborderfix/mixin/WorldMixin.java
+++ src/main/java/us/potatoboy/worldborderfix/mixin/WorldMixin.java
@@ -1,6 +1,7 @@
 package us.potatoboy.worldborderfix.mixin;
 
 import net.minecraft.util.profiler.Profiler;
+import net.minecraft.util.registry.RegistryEntry;
 import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.MutableWorldProperties;
 import net.minecraft.world.World;
@@ -16,17 +17,17 @@ import java.util.function.Supplier;
 
 @Mixin(World.class)
 public abstract class WorldMixin {
-	@Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/dimension/DimensionType;getCoordinateScale()D"))
-	private double setBorderCoordinateScale(DimensionType dimensionType) {
-		return 1.0D;
-	}
+    @Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/dimension/DimensionType;getCoordinateScale()D"))
+    private double setBorderCoordinateScale(DimensionType dimensionType) {
+        return 1.0D;
+    }
 
-	@Inject(
-			method = "<init>",
-			at = @At(value = "INVOKE", target = "Ljava/lang/Thread;currentThread()Ljava/lang/Thread;")
-	)
-	private void setWorldBorderWorld(MutableWorldProperties properties, RegistryKey<World> registryRef, DimensionType dimensionType, Supplier<Profiler> profiler, boolean isClient, boolean debugWorld, long seed, CallbackInfo ci) {
-		World world = (World) (Object) this;
-		((BorderWithWorld) world.getWorldBorder()).setWorld(world);
-	}
+    @Inject(
+            method = "<init>",
+            at = @At(value = "INVOKE", target = "Ljava/lang/Thread;currentThread()Ljava/lang/Thread;")
+    )
+    private void setWorldBorderWorld(MutableWorldProperties properties, RegistryKey<World> registryRef, RegistryEntry<DimensionType> registryEntry, Supplier<Profiler> profiler, boolean isClient, boolean debugWorld, long seed, CallbackInfo ci) {
+        World world = (World) (Object) this;
+        ((BorderWithWorld) world.getWorldBorder()).setWorld(world);
+    }
 }
